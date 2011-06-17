f575820da472d664c6b46874113418247c2b7b62
ulrik nielsen <ulrik@o3.dk>
2011-06-17 08:59:07 +0200

    cleanup

--- system/app.php
+++ system/app.php
@@ -1,12 +1,17 @@
 <?php
-// running suhosin remember to set:
-// suhosin.executor.include.whitelist="phar"
+/**
+ * if you are running suhosin remember to set:
+ * suhosin.executor.include.whitelist="phar"
+ * in your php.ini file
+ */
 
 require_once __DIR__ . '/vendor/Silex/silex.phar';
-
 $app = new Silex\Application();
 
-// templating
+
+/**
+ * setup twig
+ */
 $app->register(new Silex\Extension\TwigExtension(), array(
   'twig.path' => __DIR__ . '/templates',
   'twig.class_path' => __DIR__ . '/vendor/Twig/lib',
@@ -14,7 +19,10 @@ $app->register(new Silex\Extension\TwigExtension(), array(
   'twig.options' => array('cache' => FALSE),
 ));
 
-// database abstraction
+
+/**
+ * setup doctrine
+ */
 $app->register(new Silex\Extension\DoctrineExtension(), array(
   'db.options' => array(
     'driver' => 'pdo_sqlite',
@@ -24,52 +32,101 @@ $app->register(new Silex\Extension\DoctrineExtension(), array(
   'db.common.class_path' => __DIR__ . '/vendor/doctrine-dbal/lib/vendor/doctrine-common/lib/',
 ));
 
-// emailing
+
+/**
+ * setup swiftmailer
+ */
 $app->register(new Silex\Extension\SwiftmailerExtension(), array(
   'swiftmailer.class_path' => __DIR__ . '/vendor/swiftmailer/'
 ));
 
+
+/**
+ * setup sesion handeling
+ */
 $app->register(new Silex\Extension\SessionExtension());
 if ($app['session']->hasFlash('message')) {
   $app['twig']->addGlobal('message', $app['session']->getFlash('message'));
 }
 
 
-// favicon fail..
-$app->get('/favicon.ico', function() use ($app) {
-  header('HTTP/1.1 404 Not Found', true, 404);
-});
-
-// setup autoloader
+/**
+ * add bonzaii autoloader
+ */
 $app['autoloader']->registerNamespaces(array('Bonzaii' => __DIR__ . '/'));
 
-//register global bonzaii extentions
+
+/**
+ * register bonzaii extentions
+ */
 $app->register(new Bonzaii\ArticlesExtension(), array(
   'articles.limit' => 10,
   'articles.image_upload_path' => __DIR__ . '/../public_html/fx/img/',
   'articles.image_path' => '/fx/img/',
 ));
 $app->register(new Bonzaii\RedirectsExtension(), array());
-
 $app->register(new Bonzaii\AuthorsExtension(), array(
   'authors.image_upload_path' => __DIR__ . '/../public_html/fx/img/',
   'authors.image_path' => '/fx/img/',
 ));
 
 
-// poor mans admin switch
+/**
+ * this is a poor-mans admin switch
+ */
 if (strpos($_SERVER['REQUEST_URI'], '/admin/') === 0) {
+  $app['is_backend'] = true;
   $app['twig']->addGlobal('layout', 'backend/layout-backend.html.twig');
   require __DIR__ . '/backend.php';
 }
 else {
+  $app['is_backend'] = false;
   $app['twig']->addGlobal('layout', 'layout-frontend.html.twig');
   $app->register(new Bonzaii\TweetsExtension(), array(
     'tweets.limit' => 5,
     'tweets.search' => 'bellcomdk'
   ));
-
   require __DIR__ . '/frontend.php';
 }
+
+
+/**
+ * kill favicon requests
+ */
+$app->get('/favicon.ico', function() use ($app) {
+  header('HTTP/1.1 404 Not Found', true, 404);
+});
+
+
+/**
+ * before actions is actions handled before any request is handled
+ * note that the request is parsed by silex at this point, so any tweaks should be handled before calling on silex.
+ */
+$app->before(function() use ($app) {
+  // login check
+  if ($app['is_backend'] && !$app['access']->check()) {
+    // for some reason we cannot return an $app->redirect() here
+    header('Location: /admin/account/login');
+    exit;
+  }
+});
+
+
+/**
+ * catch errors
+ * here we use the redirects bundle to handle any known redirects
+ */
+$app->error(function(Exception $e) use ($app){
+  if ($e->getStatusCode() == 404) {
+    $path = trim($app['request']->getRequestUri(), '/');
+    if ($to = $app['redirects']->find($path)) {
+      // send 301 to tell google and others that the page has moved.
+      return $app->redirect($to, 301);
+    }
+    // send unknown requests to the error log, maby someone is watching
+    error_log('This path has no march: "' . $path . '"');
+  }
+  return $app->redirect('/');
+});
 
 return $app;
--- system/backend.php
+++ system/backend.php
@@ -1,17 +1,8 @@
 <?php
 
-// login check
-$app->before(function() use ($app) {
-  if (!$app['access']->check()) {
-    header('Location: /admin/account/login');
-    exit;
-  }
-});
-
 if (empty($app['tags'])) {
   $app->register(new Bonzaii\TagsExtension(), array());
 }
-
 $app->register(new Bonzaii\AccessExtension(), array());
 
 require __DIR__ . '/Bonzaii/Backend/app.php';
